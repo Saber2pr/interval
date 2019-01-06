d4d5300eb647ee96e4ad35198c38b96ea21d003f
saber2pr <saber2pr@gmail.com>
2019-01-06 22:09:19 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 export * from './core/saber-interval'
 
-import { schedule, FrameProps } from './core/saber-interval'
+// import { schedule, FrameProps } from './core/saber-interval'
 
-let frame: FrameProps = {
-  delta: 100,
-  isStop: false
-}
+// let frame: FrameProps = {
+//   delta: 100,
+//   isStop: false
+// }
 
-schedule(dt => console.log('update', dt), frame)
+// schedule(dt => console.log('update', dt), frame)
 
-setTimeout(() => {
-  frame.isStop = true
-}, 2000)
+// setTimeout(() => {
+//   frame.isStop = true
+// }, 2000)
 
 // schedule(() => console.log('simple update!'))
